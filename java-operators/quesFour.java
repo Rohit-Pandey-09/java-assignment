bad088f8345e27bfe747c54012c5d8a69f580378
Rohit Pandey <124355597+Rohit-Pandey-09@users.noreply.github.com>
2023-05-03 13:01:45 +0530

    Add files via upload

---
+++ java-operators/quesFour.java
@@ -0,0 +1,18 @@
+
+public class quesFour {
+public static void main(String[] args) {
+// i) both the conditions 'a < 50' and 'a < b' are true.
+
+int a = 55;
+int b = 70;
+System.out.print(a < 50 && a < b);
+
+
+//ii) at least one of the conditions 'a < 50' or 'a < b' is true.
+
+ a = 55;
+ b = 70;
+System.out.print(a < 50 || a < b);
+
+}
+}
---
+++ java-operators/quesOne.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class quesOne {
+    public static void main(String[] args) {
+Scanner scn = new Scanner(System.in);
+int x = scn.nextInt();
+System.out.print((((x + 8) / 3) % 5) * 5);
+}
+}
